0a831d27584328fc428dd970cb22c37cfa568b63
zydou <douzy94@gmail.com>
2022-12-07 12:38:49 +0800

    feat: add `set-user` feature

---
+++ src/set-user/install.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env sh
+#-------------------------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
+#-------------------------------------------------------------------------------------------------------------
+#
+# Docs: https://github.com/microsoft/vscode-dev-containers/blob/main/script-library/docs/common.md
+# Maintainer: The VS Code and Codespaces Teams
+
+set -e
+
+USERNAME="${USERNAME:-"${_REMOTE_USER:-"vscode"}"}"
+USER_UID="${UID:-"automatic"}"
+USER_GID="${GID:-"automatic"}"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+# If in automatic mode, determine if a user already exists, if not use vscode
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=vscode
+elif [ "${USERNAME}" = "none" ]; then
+    USERNAME=root
+    USER_UID=0
+    USER_GID=0
+fi
+
+# Ensure apt is in non-interactive to avoid prompts
+export DEBIAN_FRONTEND=noninteractive
+
+# Install shadow on alpine
+if [ -x "$(command -v apk)" ] ; then
+    apk add --no-cache shadow bash
+fi
+
+# Create or update a non-root user to match UID/GID.
+group_name="${USERNAME}"
+if id -u ${USERNAME} > /dev/null 2>&1; then
+    # User exists, update if needed
+    if [ "${USER_GID}" != "automatic" ] && [ "$USER_GID" != "$(id -g $USERNAME)" ]; then
+        group_name="$(id -gn $USERNAME)"
+        groupmod --gid $USER_GID ${group_name}
+        usermod --gid $USER_GID $USERNAME
+    fi
+    if [ "${USER_UID}" != "automatic" ] && [ "$USER_UID" != "$(id -u $USERNAME)" ]; then
+        usermod --uid $USER_UID $USERNAME
+    fi
+else
+    # Create user
+    if [ "${USER_GID}" = "automatic" ]; then
+        groupadd $USERNAME
+    else
+        groupadd --gid $USER_GID $USERNAME
+    fi
+    if [ "${USER_UID}" = "automatic" ]; then
+        useradd -s /bin/bash --gid $USERNAME -m $USERNAME
+    else
+        useradd -s /bin/bash --uid $USER_UID --gid $USERNAME -m $USERNAME
+    fi
+fi
+
+echo "Done!"
--- test/set-user/test.sh
+++ test/set-user/test.sh
@@ -11,12 +11,12 @@
 #    }
 # }
 #
-# Thus, the value of all options, 
+# Thus, the value of all options,
 # will fall back to the default value in the feature's 'devcontainer-feature.json'
 # For the 'color' feature, that means the default favorite color is 'red'.
-# 
+#
 # This test can be run with the following command (from the root of this repo)
-#    devcontainer features test \ 
+#    devcontainer features test \
 #               --features color \
 #               --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
 
@@ -27,7 +27,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check "version" bash -c "color | grep 'my favorite color is red'"
+check "check set-user" bash -c "id -u vscode | grep '1000'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
