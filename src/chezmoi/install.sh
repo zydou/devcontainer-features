96fe61027ffd0649929cd4de14e6d5e2e264f5f2
zydou <douzy94@gmail.com>
2022-12-20 18:17:02 +0800

    feat: add `chezmoi` feature

---
+++ src/chezmoi/install.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+# Modified from https://github.com/meaningful-ooo/devcontainer-features/tree/main/src/homebrew
+
+
+CHEZMOI_VERSION=${VERSION:-"latest"}
+DOTFILES_REPO=${DOTFILES_REPO:-"none"}
+USERNAME=${USERNAME:-"automatic"}
+BINDIR="/usr/bin"  # chezmoi installation directory
+cleanup() {
+  source /etc/os-release
+  case "${ID}" in
+    debian|ubuntu)
+      rm -rf /var/lib/apt/lists/*
+    ;;
+  esac
+}
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+  USERNAME=""
+  POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+  for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+    if id -u ${CURRENT_USER} > /dev/null 2>&1; then
+      USERNAME=${CURRENT_USER}
+      break
+    fi
+  done
+  if [ "${USERNAME}" = "" ]; then
+    USERNAME=root
+  fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} > /dev/null 2>&1; then
+  echo "user ${USERNAME} not exits, create it."
+  groupadd ${USERNAME}
+  useradd -s /bin/bash --gid $USERNAME -m $USERNAME
+fi
+
+echo "USERNAME is ${USERNAME}"
+# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
+. /etc/os-release
+# Get an adjusted ID independant of distro variants
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
+    ADJUSTED_ID="debian"
+elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* ]]; then
+    ADJUSTED_ID="rhel"
+elif [ "${ID}" = "alpine" ]; then
+    ADJUSTED_ID="alpine"
+else
+    echo "Linux distro ${ID} not supported."
+    exit 1
+fi
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+  case "${ADJUSTED_ID}" in
+      "debian")
+          if ! dpkg -s "$@" >/dev/null 2>&1; then
+            apt-get update
+            apt-get -y install --no-install-recommends "$@"
+          fi
+          ;;
+      "rhel")
+          local install_cmd=dnf
+          if ! type dnf > /dev/null 2>&1; then
+              install_cmd=yum
+          fi
+          ${install_cmd} -y install "$@"
+          ;;
+      "alpine")
+          if ! apk -e info "$@" >/dev/null 2>&1; then
+            apk add --no-cache "$@"
+          fi
+          ;;
+  esac
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Install dependencies if missing
+check_packages git curl ca-certificates
+
+# Download official install script
+curl -fsLS get.chezmoi.io -o /tmp/install_chezmoi.sh
+chmod +x /tmp/install_chezmoi.sh
+/tmp/install_chezmoi.sh -b ${BINDIR} -t ${CHEZMOI_VERSION} -d
+if [ "${DOTFILES_REPO}" != "none" ]; then
+  echo "chezmoi init && apply"
+  su ${USERNAME} bash -c "${BINDIR}/chezmoi init ${DOTFILES_REPO}"
+  su ${USERNAME} bash -c "${BINDIR}/chezmoi apply --force --debug"
+  # Do it again as some scripts unexpected failed
+  su ${USERNAME} bash -c "${BINDIR}/chezmoi apply --force --debug"
+fi
+
+# Persist ENV
+echo "export DEVCONTAINER_FEATURE_CHEZMOI=1" > /etc/profile.d/99-chezmoi.sh
+chmod +x /etc/profile.d/99-chezmoi.sh
+
+cleanup
+cd $HOME
+rm -rf ./cache
+rm -f /tmp/install_chezmoi.sh
+echo "Done!"
---
+++ test/chezmoi/test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# This test file will be executed against an auto-generated devcontainer.json that
+# includes the 'color' feature with no options.
+#
+# Eg:
+# {
+#    "image": "<..some-base-image...>",
+#    "features": {
+#      "color": {}
+#    }
+# }
+#
+# Thus, the value of all options,
+# will fall back to the default value in the feature's 'devcontainer-feature.json'
+# For the 'color' feature, that means the default favorite color is 'red'.
+#
+# This test can be run with the following command (from the root of this repo)
+#    devcontainer features test \
+#               --features feature-id \
+#               --base-image mcr.microsoft.com/devcontainers/base:ubuntu .
+
+set -e
+
+# # Optional: Import test library bundled with the devcontainer CLI
+# source dev-container-features-test-lib
+
+# # Feature-specific tests
+# # The 'check' command comes from the dev-container-features-test-lib.
+# check "check name" bash -c "id -u vscode | grep '1000'"
+
+# # Report result
+# # If any of the checks above exited with a non-zero exit code, the test will fail.
+# reportResults
+
+NON_ROOT_USER=""
+POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+  if id -u ${CURRENT_USER} >/dev/null 2>&1; then
+    NON_ROOT_USER=${CURRENT_USER}
+    break
+  fi
+done
+if [ "${NON_ROOT_USER}" = "" ]; then
+  NON_ROOT_USER=root
+fi
+
+su "$NON_ROOT_USER" -c "chezmoi --version"
+ret=$?
+if [ "$ret" -eq '0' ]; then
+    echo '✅ Passed(chezmoi): chezmoi installed successfully'
+    exit 0
+else
+    echo '❌ Failed(chezmoi): chezmoi not found!'
+    exit 1
+fi
