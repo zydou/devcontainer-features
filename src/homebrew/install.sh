1ab317c72bec58a778a8d8111773a41ec851f699
zydou <douzy94@gmail.com>
2022-12-28 00:44:42 +0800

    fix: check `/etc/bash.bashrc` exists before `updaterc`

--- src/homebrew/install.sh
+++ src/homebrew/install.sh
@@ -77,7 +77,7 @@ check_packages() {
 
 updaterc() {
   echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
-  if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+  if [ -f "/etc/bash.bashrc" ] && [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
       echo -e "$1" >> /etc/bash.bashrc
   fi
   if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
--- src/python-pkgs/install.sh
+++ src/python-pkgs/install.sh
@@ -38,7 +38,7 @@ fi
 
 updaterc() {
     echo "Updating /etc/bash.bashrc and /etc/zsh/zshrc..."
-    if [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
+    if [ -f "/etc/bash.bashrc" ] && [[ "$(cat /etc/bash.bashrc)" != *"$1"* ]]; then
         echo -e "$1" >> /etc/bash.bashrc
     fi
     if [ -f "/etc/zsh/zshrc" ] && [[ "$(cat /etc/zsh/zshrc)" != *"$1"* ]]; then
